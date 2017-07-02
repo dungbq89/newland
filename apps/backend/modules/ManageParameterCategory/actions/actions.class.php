774a49a8504194cfc0c661ebe438d882611cbae3
conguy20hn <conghuy20hn@gmail.com>
2017-07-02 09:46:27 +0700

    module parameter + fix insert lang

---
+++ apps/backend/modules/ManageParameterCategory/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/ManageParameterCategoryGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/ManageParameterCategoryGeneratorHelper.class.php';
+
+/**
+ * ManageParameterCategory actions.
+ *
+ * @package    Web_Portals
+ * @subpackage ManageParameterCategory
+ * @author     ngoctv1
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ManageParameterCategoryActions extends autoManageParameterCategoryActions
+{
+}
