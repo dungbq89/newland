cca5ae74a88a0adf14ab28522bf525a8305f8954
dungbq89 <dungbq89@gmail.com>
2017-07-11 22:38:00 +0700

    sua duong dan anh

--- apps/frontend/modules/pageHome/templates/_slide.php
+++ apps/frontend/modules/pageHome/templates/_slide.php
@@ -14,14 +14,11 @@ if (!empty($listSlide)) {
                 <?php foreach ($listSlide as $item){ ?>
                 <li style="position:relative">
                     <a href="<?php echo $item['link'] != '' ? $item['link'] : 'javascript:void(0)' ?>" target=""><img
-                            src="<?php echo VtHelper::getPathImage($item['file_path'], sfConfig::get('app_advertise_images', 'advertise')) ?>"
+                            src="<?php echo VtHelper::getImagePath($item['file_path'], sfConfig::get('app_advertise_images', 'advertise')) ?>"
                             alt="Slide 5 - English"
                             width="100%"/></a>
                     <?php } ?>
                 </li>
-                <!--                <li style="position:relative">-->
-                <!--                    <a href="" target=""><img src="/images/slideshow/1.jpg" alt="Slide 5 - English"-->
-                <!--                                              width="100%"/></a>-->
 
                 </li>
             </ul>
