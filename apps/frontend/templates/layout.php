50606aee07c481280bdc4468f3f61d284891a5ea
dungbq89 <dungbq89@gmail.com>
2017-07-23 22:43:14 +0700

    đa ngôn ngữ

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Newland Serviced Apartment</title>
+    <title>Nehob Serviced Apartment</title>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
@@ -12,13 +12,13 @@
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0, user-scalable=yes">
     <meta http-equiv="Content-Language" content=""/>
-    <meta name="description" content="Newland Serviced Apartment"/>
+    <meta name="description" content="Nehob Serviced Apartment"/>
     <meta name="keywords" content="serviced apartment, studio apartment, cheap serviced apartment"/>
     <meta property="og:site_name" content="companyname">
     <meta property="og:url" name="og:url" content="" data-app>
     <meta property="og:type" name="og:type" content="website" data-app>
-    <meta property="og:description" name="og:description" content="Newland Serviced Apartment" data-app>
-    <meta property="og:title" name="og:title" content="Newland Serviced Apartment" data-app>
+    <meta property="og:description" name="og:description" content="Nehob Serviced Apartment" data-app>
+    <meta property="og:title" name="og:title" content="Nehob Serviced Apartment" data-app>
     <meta property="og:image" name="og:image" content="" data-app>
     <link href="favicon.png" rel="shortcut icon"/>
     <meta http-equiv="REFRESH" content="5400"/>
